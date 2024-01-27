18cc6101a84abdb0093b43095333ff32819a7513
Jihwan Moon <dev.moonjihwan@gmail.com>
2024-01-27 17:50:22 +0900

    Edit example code

--- Example/Example.cs
+++ Example/Example.cs
@@ -1,31 +1,49 @@
-﻿using MapleStory.NET;
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using MapleStory.NET;
 
 var apiKey = "Your_api_key_here";
-
 using var httpClient = new HttpClient();
+
 var client = new MapleStoryClient(httpClient, apiKey);
+
 var overallRankingResult = await client.RankingApi.GetOverallRankingAsync(); //fetch overall ranking
 
-if (!overallRankingResult.Success)
+if (overallRankingResult.Data is null)
 {
     Console.WriteLine(overallRankingResult.Error);
     return;
 }
 
-var top10 = overallRankingResult.Data!.Ranking!.Take(10); //get top 10 characters
-var firstPlace = top10.First(); //get first place
-var characterResult = await client.CharacterApi.GetAsync(firstPlace.CharacterName!); //fetch character identifier(ocid)
+var top10 = overallRankingResult.Data.Ranking?.Take(10); //get top 10 characters
+var firstPlace = top10.FirstOrDefault(); //get first place
 
-if (!characterResult.Success)
+if (firstPlace?.CharacterName is null)
+{
+    Console.WriteLine("Unable to get first place.");
+    return;
+}
+
+var characterResult = await client.CharacterApi.GetAsync(firstPlace.CharacterName); //fetch character identifier(ocid)
+
+if (characterResult.Data is null)
 {
     Console.WriteLine(characterResult.Error);
     return;
 }
 
-var ocid = characterResult.Data!.Ocid!;
+var ocid = characterResult.Data.Ocid;
+
+if (ocid is null)
+{
+    Console.WriteLine("Unable to get ocid.");
+    return;
+}
+
 var characterBasicResult = await client.CharacterApi.GetBasicAsync(ocid); //fetch basic data
 
-if (!characterBasicResult.Success)
+if (characterBasicResult.Data is null)
 {
     Console.WriteLine(characterBasicResult.Error);
     return;
