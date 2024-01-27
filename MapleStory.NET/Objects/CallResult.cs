929c1c550fec17ded5d5595732b186136edd04f6
Jihwan Moon <dev.moonjihwan@gmail.com>
2024-01-27 17:53:15 +0900

    Cleanup unnecessary code

--- MapleStory.NET/Objects/CallResult.cs
+++ MapleStory.NET/Objects/CallResult.cs
@@ -2,46 +2,28 @@ namespace MapleStory.NET.Objects;
 /// <summary>
 /// 호출 결과
 /// </summary>
-/// <param name="error">발생한 에러</param>
-public class CallResult(Error? error)
-{
-    /// <summary>
-    /// 발생한 에러
-    /// </summary>
-    public Error? Error { get; set; } = error;
-    /// <summary>
-    /// 성공 여부
-    /// </summary>
-    public bool Success => Error is null;
-    /// <summary>
-    /// 성공 여부
-    /// </summary>
-    /// <param name="obj"></param>
-
-    public static implicit operator bool(CallResult obj) => obj?.Success == true;
-    /// <summary>
-    /// 문자열 반환을 위한 ToString 구현
-    /// </summary>
-    /// <returns>성공 시 Success, 실패 시 Error: {Error} 반환</returns>
-    public override string ToString() => Success ? "Success" : $"Error: {Error}";
-}
-
-/// <summary>
-/// 호출 결과
-/// </summary>
 /// <typeparam name="T">반환할 데이터의 타입</typeparam>
-public class CallResult<T> : CallResult
+public class CallResult<T>
 {
+    /// <summary>
+    /// 발생한 에러. 성공 시 null이 들어간다.
+    /// </summary>
+    public Error? Error { get; }
     /// <summary>
     /// 성공 시 반환할 데이터
     /// </summary>
-    public T? Data { get; internal set; }
+    public T? Data { get; set; }
+
     /// <summary>
     /// 생성자
     /// </summary>
     /// <param name="data">데이터</param>
     /// <param name="error">에러</param>
-    protected CallResult(T? data, Error? error) : base(error) => Data = data;
+    protected CallResult(T? data, Error? error)
+    {
+        Data = data;
+        Error = error;
+    }
     /// <summary>
     /// 생성자
     /// </summary>
@@ -52,10 +34,10 @@ public class CallResult<T> : CallResult
     /// </summary>
     /// <param name="error">에러</param>
     public CallResult(Error error) : this(default, error) { }
-    /// <summary>
-    /// 성공 여부
-    /// </summary>
-    /// <param name="obj"></param>
-    public static implicit operator bool(CallResult<T> obj) => obj?.Success == true;
-}
 
+    /// <summary>
+    /// 문자열 반환을 위한 ToString 구현
+    /// </summary>
+    /// <returns>성공 시 Success, 실패 시 Error: {Error} 반환</returns>
+    public override string ToString() => Data is not null ? "Success" : $"Error: {Error}";
+}
